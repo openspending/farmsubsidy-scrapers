e09318d4d114e7d84c9fa9f899828785bc9285c6
cyberbikepunk <loic.jounot@gmail.com>
2016-05-24 13:18:41 +0200

    Set a fake user agent to make the Croatian government happy.

--- scrapy_fs/scrapy_fs/settings.py
+++ scrapy_fs/scrapy_fs/settings.py
@@ -18,3 +18,4 @@ NEWSPIDER_MODULE = 'scrapy_fs.spiders'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'scrapy_fs (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36'
