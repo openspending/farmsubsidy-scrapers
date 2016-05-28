e9ec9476eff085d743f0d4a247a3b3fc2cc754dc
cyberbikepunk <loic.jounot@gmail.com>
2016-05-28 15:24:04 +0200

    Refactor the pagination mechanism into a seperate class.

--- scrapy_fs/scrapy_fs/spiders/lt_spider.py
+++ scrapy_fs/scrapy_fs/spiders/lt_spider.py
@@ -5,39 +5,64 @@ from scrapy import Request
 from scrapy.spiders.crawl import Spider
 
 
+# Translations
+YEAR = 'fin_metai'
+COUNTER = 'psl_nr'
+
+
 class LithuaniaSpider(Spider):
     name = 'lithuania'
     country = 'LT'
     allowed_domains = ['portal.nma.lt']
-    base_url = 'https://portal.nma.lt/nma-portal/pages/fas_search'
 
-    X_NEXT_LINK = './/a[contains(@href, "showPage") and contains(text(), "Kitas")]'
-    RE_PAGE_COUNT = r'showPage\((\d+)\)'
-
-    def __init__(self, year=2015, *args, **kwargs):
-        self.year = str(year)
-        self.page_count = 1
-        self.query ={
-            'pa': 'pl',
-            'pTipas': 'p',
-            'programos_kodas': 'KP13',
-            'action': 'Search',
-            'fin_metai': self.year,
-        }
-        super(LithuaniaSpider, self).__init__(*args, **kwargs)
-
-    @property
-    def pagination_url(self):
-        self.query.update({'psl_nr': str(self.page_count)})
-        query_string = [key + '=' + value for key, value in self.query.items()]
-        return self.base_url + '?' + '&'.join(query_string)
+    def __init__(self, year=2015):
+        self.year = year
+        self.page = Paginator(year)
+        super(LithuaniaSpider, self).__init__()
 
     def start_requests(self):
-        return [Request(self.pagination_url, method='POST')]
+        return [self.page.request]
 
     def parse(self, response):
-        self.logger.debug('Landed on %s', response.url)
-        page_count = response.xpath(self.X_NEXT_LINK).re_first(self.RE_PAGE_COUNT)
-        if page_count:
-            self.page_count = page_count
-        return Request(self.pagination_url, method='POST')
+        self.logger.debug('Landed on page %s', self.page.counter)
+
+        if self.page.has_more(response):
+            yield self.page.request
+
+
+class Paginator(object):
+    base_url = 'https://portal.nma.lt/nma-portal/pages/fas_search'
+    query = {
+        'pa': 'pl',
+        'pTipas': 'p',
+        'programos_kodas':
+        'KP13', 'action':
+        'Search',
+        'psl_nr': '1',
+        'fin_metai': '2015'
+    }
+
+    X_LINK = './/a[contains(@href, "showPage") and contains(text(), "Kitas")]'
+    RE_COUNTER = r'showPage\((\d+)\)'
+
+    def __init__(self, year):
+        self.update(YEAR, year)
+
+    def update(self, key, value):
+        self.query.update({key: str(value)})
+
+    def has_more(self, response):
+        more = response.xpath(self.X_LINK).re_first(self.RE_COUNTER)
+        self.update(COUNTER, more)
+        return True if more else False
+
+    @property
+    def request(self):
+        parameters = [key + '=' + value for key, value in self.query.items()]
+        url = self.base_url + '?' + '&'.join(parameters)
+        return Request(url, method='POST')
+
+    @property
+    def counter(self):
+        return self.query[COUNTER]
+
