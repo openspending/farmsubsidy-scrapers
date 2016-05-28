aef7791e1426874d71b7791ddf1bd2e2ec7526d0
cyberbikepunk <loic.jounot@gmail.com>
2016-05-28 14:09:37 +0200

    Add pagination crawling super-powers to the lithuanian spider.

--- scrapy_fs/scrapy_fs/spiders/lt_spider.py
+++ scrapy_fs/scrapy_fs/spiders/lt_spider.py
@@ -1,32 +1,43 @@
 """ This is the scraper for Lithuania. """
 
 
-from scrapy.spiders import CrawlSpider
-from scrapy import FormRequest
-from scrapy.utils.response import open_in_browser
+from scrapy import Request
+from scrapy.spiders.crawl import Spider
 
 
-class LithuaniaSpider(CrawlSpider):
+class LithuaniaSpider(Spider):
     name = 'lithuania'
     country = 'LT'
+    allowed_domains = ['portal.nma.lt']
+    base_url = 'https://portal.nma.lt/nma-portal/pages/fas_search'
 
-    SEARCH_URL = 'https://portal.nma.lt/nma-portal/pages/fas_search'
-    SEARCH_QUERY = {
-        'pa': 'pl',
-        'pTipas': 'p',
-        'psl_nr': '1',
-        'programos_kodas': 'KP13',
-        'fin_metai': '2014',
-        'action': 'Search'
-    }
+    X_NEXT_LINK = './/a[contains(@href, "showPage") and contains(text(), "Kitas")]'
+    RE_PAGE_COUNT = r'showPage\((\d+)\)'
 
     def __init__(self, year=2015, *args, **kwargs):
-        self.year = year
+        self.year = str(year)
+        self.page_count = 1
+        self.query ={
+            'pa': 'pl',
+            'pTipas': 'p',
+            'programos_kodas': 'KP13',
+            'action': 'Search',
+            'fin_metai': self.year,
+        }
         super(LithuaniaSpider, self).__init__(*args, **kwargs)
 
+    @property
+    def pagination_url(self):
+        self.query.update({'psl_nr': str(self.page_count)})
+        query_string = [key + '=' + value for key, value in self.query.items()]
+        return self.base_url + '?' + '&'.join(query_string)
+
     def start_requests(self):
-        self.SEARCH_QUERY.update({'fin_metai': str(self.year)})
-        return [FormRequest(self.SEARCH_URL, formdata=self.SEARCH_QUERY)]
+        return [Request(self.pagination_url, method='POST')]
 
     def parse(self, response):
-        open_in_browser(response)
+        self.logger.debug('Landed on %s', response.url)
+        page_count = response.xpath(self.X_NEXT_LINK).re_first(self.RE_PAGE_COUNT)
+        if page_count:
+            self.page_count = page_count
+        return Request(self.pagination_url, method='POST')
