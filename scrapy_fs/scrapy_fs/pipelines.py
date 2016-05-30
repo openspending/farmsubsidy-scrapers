c46d29c8b6c19e326f48d41dcf4737964c05badb
cyberbikepunk <loic.jounot@gmail.com>
2016-05-30 17:35:36 +0200

    Drop subsidies where the amount is equal to 0.

--- scrapy_fs/scrapy_fs/pipelines.py
+++ scrapy_fs/scrapy_fs/pipelines.py
@@ -3,13 +3,21 @@
 from scrapy.exceptions import DropItem
 
 
-class CroatianNationalSubsidyFilter(object):
+class DropSubsidyFilter(object):
     """ Filters out subsidy items corresponding to national schemes. """
 
     @staticmethod
     def process_item(item, spider):
         args = (spider.name, item['scheme'], item['recipient_name'])
-        if 'nacionalna' in item['agency'].lower():
-            raise DropItem('%s dropped national subsidy item "%s" for %s' % args)
-        else:
-            return item
+
+        if spider.name == 'croatia':
+            if 'nacionalna' in item['agency'].lower():
+                raise DropItem('%s dropped national subsidy item "%s" for %s' % args)
+            else:
+                return item
+
+        if spider.name == 'lithuania':
+            if not item['amount']:
+                raise DropItem('%s dropped empty subsidy item "%s" for %s' % args)
+            else:
+                return item
